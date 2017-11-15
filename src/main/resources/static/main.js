6c0e9f2851900d542b8ff48e573ac611801b2cf7
tianshouzhi <tianshouzhi@meituan.com>
2017-11-15 22:38:20 +0800

    init

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,238 @@
+/***
+ AngularJS App Main Script
+***/
+
+/* App */
+var APP = angular.module("APP", [
+    "ui.router", 
+    "ui.bootstrap", 
+    "oc.lazyLoad",  
+    "ngSanitize"
+]); 
+
+/* Configure ocLazyLoader(refer: https://github.com/ocombe/ocLazyLoad) */
+APP.config(['$ocLazyLoadProvider', function($ocLazyLoadProvider) {
+    $ocLazyLoadProvider.config({
+        // global configs go here
+    });
+}]);
+
+//AngularJS v1.3.x workaround for old style controller declarition in HTML
+APP.config(['$controllerProvider', function($controllerProvider) {
+  // this option might be handy for migrating old apps, but please don't use it
+  // in new ones!
+  $controllerProvider.allowGlobals();
+}]);
+
+/********************************************
+ END: BREAKING CHANGE in AngularJS v1.3.x:
+*********************************************/
+
+/* Setup global settings */
+APP.factory('settings', ['$rootScope', function($rootScope) {
+    // supported languages
+    var settings = {
+        layout: {
+            pageSidebarClosed: false, // sidebar menu state
+            pageContentWhite: true, // set page content layout
+            pageBodySolid: false, // solid body color state
+            pageAutoScrollOnLoad: 1000 // auto scroll to top on page load
+        }
+    };
+
+    $rootScope.settings = settings;
+
+    return settings;
+}]);
+
+/* Setup App Main Controller */
+APP.controller('AppController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.$on('$viewContentLoaded', function() {
+        //App.initComponents(); // init core components
+        //Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive 
+    });
+}]);
+
+/***
+Layout Partials.
+By default the partials are loaded through AngularJS ng-include directive. In case they loaded in server side(e.g: PHP include function) then below partial 
+initialization can be disabled and Layout.init() should be called on page load complete as explained above.
+***/
+
+/* Setup Layout Part - Header */
+APP.controller('HeaderController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initHeader(); // init header
+    });
+}]);
+
+/* Setup Layout Part - Sidebar */
+APP.controller('SidebarController', ['$state', '$scope', function($state, $scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initSidebar($state); // init sidebar
+    });
+}]);
+
+/* Setup Layout Part - Quick Sidebar */
+APP.controller('QuickSidebarController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+       setTimeout(function(){
+            QuickSidebar.init(); // init quick sidebar        
+        }, 2000)
+    });
+}]);
+
+
+/* Setup Layout Part - Footer */
+APP.controller('FooterController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initFooter(); // init footer
+    });
+}]);
+
+/* Setup Rounting For All Pages */
+APP.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    // Redirect any unmatched url
+    $urlRouterProvider.otherwise("/dashboard.html");
+
+    $stateProvider
+
+        // Dashboard
+        .state('dashboard', {
+            url: "/dashboard.html",
+            templateUrl: "templates/dashboard/dashboard.html",
+            data: {pageTitle: 'Admin Dashboard Template'},
+            controller: "DashboardController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'APP',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+                        files: [
+                            'bower_components/morris.js/morris.css',
+                            'bower_components/morris.js/morris.min.js',
+                            'bower_components/raphael/raphael.min.js',
+                            'bower_components/jquery-sparkline/dist/jquery.sparkline.min.js',
+
+                            'templates/dashboard/dashboard.js',
+                            'templates/dashboard/DashboardController.js',
+                        ] 
+                    });
+                }]
+            }
+        })
+
+        // Blank Page
+        .state('blank', {
+            url: "/blank",
+            templateUrl: "templates/blank/blank.html",
+            data: {pageTitle: 'Blank Page Template'},
+            controller: "BlankController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'APP',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+                        files: [
+                            'templates/blank/BlankController.js'
+                        ] 
+                    });
+                }]
+            }
+        })
+
+
+        // Form Tools
+        .state('formtools', {
+            url: "/form-tools",
+            templateUrl: "templates/form-tools/form_tools.html",
+            data: {pageTitle: 'Form Tools'},
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load([{
+                        name: 'APP',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [
+                            'bower_components/bootstrap-fileinput/css/fileinput.min.css',
+                            'bower_components/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css',
+                            'bower_components/bootstrap-markdown/css/bootstrap-markdown.min.css',
+                            '../assets/global/plugins/typeahead/typeahead.css',
+
+                            '../assets/global/plugins/fuelux/js/spinner.min.js',
+                            'bower_components/bootstrap-fileinput/js/fileinput.min.js',
+                            '../assets/global/plugins/jquery-inputmask/jquery.inputmask.bundle.min.js',
+                            '../assets/global/plugins/jquery.input-ip-address-control-1.0.min.js',
+                            'bower_components/pwstrength-bootstrap/dist/pwstrength-bootstrap.min.js',
+                            'bower_components/bootstrap-switch/dist/js/bootstrap-switch.min.js',
+                            'bower_components/bootstrap-maxlength/src/bootstrap-maxlength.js',
+                            'bower_components/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js',
+                            '../assets/global/plugins/typeahead/handlebars.min.js',
+                            '../assets/global/plugins/typeahead/typeahead.bundle.min.js',
+                            '../assets/pages/scripts/components-form-tools-2.min.js'
+                        ] 
+                    }]);
+                }] 
+            }
+        })
+
+        // Advanced Datatables
+        .state('datatablesmanaged', {
+            url: "/datatables/managed.html",
+            templateUrl: "templates/datatables/managed.html",
+            data: {pageTitle: 'Advanced Datatables'},
+            controller: "GeneralPageController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'APP',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [                             
+                            '../assets/global/plugins/datatables/datatables.min.css', 
+                            '../assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css',
+
+                            '../assets/global/plugins/datatables/datatables.all.min.js',
+
+                            '../assets/pages/scripts/table-datatables-managed.min.js',
+
+                            '/controllers/GeneralPageController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+
+        // Ajax Datetables
+        .state('datatablesajax', {
+            url: "/datatables/ajax.html",
+            templateUrl: "templates/datatables/ajax.html",
+            data: {pageTitle: 'Ajax Datatables'},
+            controller: "GeneralPageController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'APP',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [
+                            '../assets/global/plugins/datatables/datatables.min.css', 
+                            '../assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css',
+                            '../assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css',
+
+                            '../assets/global/plugins/datatables/datatables.all.min.js',
+                            '../assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js',
+                            '../assets/global/scripts/datatable.js',
+
+                            'js/scripts/table-ajax.js',
+                            '/controllers/GeneralPageController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+
+}]);
+
+/* Init global settings and run the app */
+APP.run(["$rootScope", "settings", "$state", function($rootScope, settings, $state) {
+    $rootScope.$state = $state; // state to be accessed from view
+    $rootScope.$settings = settings; // state to be accessed from view
+}]);
